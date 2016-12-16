2f5d2ee7d070c8531edf404cad17758c50a18370
mephenor <zajac.thomas1992@gmail.com>
2016-12-16 16:38:19 +0100

    Fix
    runModelPolisher.sh should now work with the current version.

--- scripts/runModelPolisher.sh
+++ scripts/runModelPolisher.sh
@@ -7,22 +7,61 @@
 ## Find location of this script ##
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-${DIR}/ModelPolisher.sh \
---user=[USER NAME] \
-[
-OPTIONAL:
---host=[HOST NAME] \
---port=[PORT] \
-]
---dbname=[DATABASE NAME] \
---passwd=[DB USER'S PASSWORD] \
---input=[INPUT FILE or DIRECTORY] \
---output=[OUTPUT FILE or DIRECTORY] \
---compression-type=GZIP \
---check-mass-balance=true \
---sbml-validation=true \
---omit-generic-terms=false \
---log-level=INFO \
---log-file=${DIR}/model_polisher.log 2>/dev/null
 
+#Default argument values
+ANNOTATE=false
+COMPRESSION_TYPE=GZIP
+CHECK_MASS_BALANCE=true
+SBML_VALIDATION=true
+OMIT_GENERIC_TERMS=false
+LOG_LEVEL=INFO
+LOG_FILE=${DIR}/model_polisher.log 2>/dev/null
+
+for OPT in $*; do
+  case $OPT in
+    --user=*) USER="${OPT#*=}";;
+    --dbname=*) DBNAME="${OPT#*=}";;
+    --annotate-with-bigg=*) ANNOTATE="${OPT#*=}";;
+    --passwd=*) PASSWD="${OPT#*=}";;
+    --host=*) HOST="${OPT#*=}";;
+    --port=*) PORT="${OPT#*=}";;
+    --input=*) INPUT="${OPT#*=}";;
+    --output=*) OUTPUT="${OPT#*=}";;
+    --compression-type=*) COMPRESSION_TYPE="${OPT#*=}";;
+    --check-mass-balance=*) CHECK_MASS_BALANCE="${OPT#*=}";;
+    --sbml-validation=*) SBML_VALIDATION="${OPT#*=}";;
+    --sbml-generic-terms=*) SBML_GENERIC_TERMS="${OPT#*=}";;
+    --log-level=*) LOG_LEVEL="${OPT#*=}";;
+    --log-file=*) LOG_FILE="${OPT#*=}";;
+  esac
+done
+
+if [ $ANNOTATE ] 
+then
+${DIR}/ModelPolisher.sh \
+--user=$USER \
+--dbname=$DBNAME \
+--annotate-with-bigg=$ANNOTATE \
+--passwd=$PASSWD \
+--host=$HOST \
+--port=$PORT \
+--input=$INPUT \
+--output=$OUTPUT \
+--compression-type=$COMPRESSION_TYPE \
+--check-mass-balance=$CHECK_MASS_BALANCE \
+--sbml-validation=$SBML_VALIDATION \
+--omit-generic-terms=$OMIT_GENERIC_TERMS \
+--log-level=$LOG_LEVEL \
+--log-file=$LOG_FILE
+else
+${DIR}/ModelPolisher.sh \
+--input=$INPUT \
+--output=$OUTPUT \
+--compression-type=$COMPRESSION_TYPE \
+--check-mass-balance=$CHECK_MASS_BALANCE \
+--sbml-validation=$SBML_VALIDATION \
+--omit-generic-terms=$OMIT_GENERIC_TERMS \
+--log-level=$LOG_LEVEL \
+--log-file=$LOG_FILE
+fi
 exit 0
