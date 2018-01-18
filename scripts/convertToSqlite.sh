ade784a496726fdee7a968b30a7cc408b4d9f07e
Thomas Jakob Zajac <zajac.thomas1992@gmail.com>
2018-01-18 20:53:14 +0100

    Changed DB conversion scripts for more automatisation.\nIndices and need a bit more work, though.

--- scripts/convertToSqlite.sh
+++ scripts/convertToSqlite.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Started conversion of local PostgreSQL BiGGDB to SQLite..." \
+&& pg_dump --schema-only bigg > bigg_schema.sql \
+&& echo "Finished dumping PostgreSQL DB schema" \
 && pg_dump --data-only --column-inserts bigg > bigg.sql \
-&& echo "Finished dumping PostgreSQL DB." \
+&& echo "Finished dumping PostgreSQL DB data." \
 && python cleanup_dump.py \
 && echo "Finished cleanup for migration to SQLite." \
-&& sqlite3 "" ".read create_db.sql" \
+&& sqlite3 "" ".read schema.sql" ".read converted.sql" ".save bigg.sqlite" \
 && echo "Finished creating SQLite DB." \
 && mv bigg.sqlite ../resources/edu/ucsd/sbrg/bigg \
-&& echo "Creating indices." \
-&& python createIndices.py \
-&& rm bigg.sql converted.sql \
 && echo "Removing temporary files..." \
+&& rm bigg.sql converted.sql bigg_schema.sql schema.sql \
 && echo "Finished."
