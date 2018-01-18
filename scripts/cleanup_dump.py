742383e8a73a224a2c61920d7e9753db4e153c96
Thomas Jakob Zajac <zajac.thomas1992@gmail.com>
2018-01-18 21:13:55 +0100

    Fixed BiGG conversion scripts. Moved SQLite DB out of the project to a Dropbox folder, configureSQLiteDB fetches it automatically. Moved dev folder out of resources. Updated BiGG to version 1.5.

--- scripts/cleanup_dump.py
+++ scripts/cleanup_dump.py
@@ -1,76 +1,113 @@
 #!/bin/python3
 
 import re
+import os
+
+parens = re.compile("\(.*\);")
+using = re.compile("USING.*")
+
 
 def main():
+    clean_schema()
+    clean_data()
 
-    with open("bigg.sql") as file:
-        content = file.readlines()
-    output = []
-    for line in content:
-        if line.startswith("--") or line.startswith("SET") or line.startswith("COPY") or "SELECT pg_catalog.setval(" in line:
-            line = ""
-        line = line.replace("true", "1")
-        line = line.replace("false", "0")
-        output.append(line)
-    with open("converted.sql", "w") as db:
-        db.write("".join(output))
 
-    parens = re.compile("\(.*\);")
-    using = re.compile("USING.*")
-
+def clean_schema():
     with open("bigg_schema.sql") as file:
         content = file.readlines()
     output = []
-    type_block = False
+    block = False
     function_block = False
     function_end = False
-    sequence_block = False
-    alter_table_block = False
+    found_cobra = False
+    found_start = False
     for line in content:
-        if type_block:
-            if ");" in line:
-                type_block = False
-        elif function_block:
+
+        # only use the public schema for now
+        if not found_cobra:
+            if line.startswith("SET search_path = cobradb, pg_catalog;"):
+                found_cobra = True
+            continue
+        if not found_start:
+            if line.startswith("SET search_path = public, pg_catalog;"):
+                found_start = True
+            continue
+
+        if function_block:
             if function_end and line.strip() == '':
                 function_block = False
                 function_end = False
             if not line.startswith(" ") and ";" in line:
                 function_end = True
-        elif sequence_block:
+            continue
+
+        elif block:
             if ";" in line:
-                sequence_block = False
-        elif alter_table_block:
-            if ";" in line:
-                alter_table_block = False
-        elif line.startswith("--") or line.startswith("SET") or line.startswith("COPY") or "SELECT pg_catalog.setval(" in line\
-                or line.startswith("CREATE EXTENSION") or line.startswith("COMMENT") or "OWNER" in line:
-            pass
-        elif line.startswith("CREATE TYPE") or line.startswith("ALTER TYPE"):
-            if not line.endswith(";"):
-                type_block = True
-        elif line.startswith("CREATE FUNCTION") or line.startswith("ALTER FUNCTION"):
-            if not line.endswith(";"):
-                function_block = True
-        elif line.startswith("CREATE SEQUENCE") or line.startswith("ALTER SEQUENCE"):
-            if not line.endswith(";"):
-                sequence_block = True
-        elif line.startswith("ALTER TABLE ONLY"):
-            if not line.endswith(";"):
-                alter_table_block = True
+                block = False
+            continue
+
+        elif is_var(line, "TYPE") or is_var(line, "SEQUENCE") or line.startswith("ALTER TABLE ONLY"):
+            block = is_block(line)
+            continue
+
+        elif is_var(line, "FUNCTION"):
+            function_block = is_block(line)
+            continue
+
         elif line.startswith("CREATE INDEX"):
-            line = line.replace("CREATE INDEX", "CREATE INDEX IF NOT EXISTS")
-            matches = re.search(parens, line)
-            match = matches.group(0)
-            match = match.replace(" gin_trgm_ops", "")
-            line = re.sub(using, match, line)
+            line = fix_index(line)
+        elif line.startswith("--") \
+                or line.strip() == "" \
+                or line.startswith("SET") \
+                or line.startswith("COPY") \
+                or line.startswith("CREATE EXTENSION") \
+                or line.startswith("COMMENT") \
+                or "OWNER" in line \
+                or "SELECT pg_catalog.setval(" in line \
+                or "SCHEMA" in line:
+            line = ""
+
+        line = line.replace("true", "1")
+        line = line.replace("false", "0")
+        if line.strip() != "":
             output.append(line)
-        else:
-            line = line.replace("true", "1")
-            line = line.replace("false", "0")
-            if line.strip() != "":
-                output.append(line)
+
     with open("schema.sql", "w") as db:
+        db.write("".join(output))
+
+
+def is_var(line, var):
+    return line.startswith("CREATE {}".format(var)) or line.startswith("ALTER {}".format(var))
+
+
+def is_block(line):
+    return not line.endswith(";")
+
+
+def fix_index(line):
+    line = line.replace("CREATE INDEX", "CREATE INDEX IF NOT EXISTS")
+    matches = re.search(parens, line)
+    match = matches.group(0)
+    match = match.replace(" gin_trgm_ops", "")
+    return re.sub(using, match, line)
+
+
+def clean_data():
+    with open("bigg.sql") as file:
+        content = file.readlines()
+    output = []
+    for line in content:
+        if line.startswith("--") \
+                or line.startswith("SET") \
+                or line.startswith("COPY") \
+                or "SELECT pg_catalog.setval(" in line \
+                or "database_version" in line:
+            line = ""
+        line = line.replace("true", "1")
+        line = line.replace("false", "0")
+        if line.strip() != "":
+            output.append(line)
+    with open("converted.sql", "w") as db:
         db.write("".join(output))
 
 
--- scripts/configureSQLiteDB.sh
+++ scripts/configureSQLiteDB.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ ! -f ../resources/edu/ucsd/sbrg/bigg/bigg.zip ]; then
+    curl -L -o bigg.zip https://www.dropbox.com/s/0j3pozc7a0p2b94/bigg.zip\?dl\=0
+    mv bigg.zip ../resources/edu/ucsd/sbrg/bigg
+fi
+
 unzip -od ../resources/edu/ucsd/sbrg/bigg ../resources/edu/ucsd/sbrg/bigg/bigg.zip
+python3 createIndices.py
