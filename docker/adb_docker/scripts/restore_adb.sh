aaeddc60c7c3cb8e3b7e3da8b802785f5a77d170
codekaust <kaustubh1303@gmail.com>
2019-07-14 00:23:41 +0530

    small updates in restore scripts

--- docker/adb_docker/scripts/restore_adb.sh
+++ docker/adb_docker/scripts/restore_adb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-createuser -s adb
-createdb adb
-psql -c "ALTER USER postgres WITH PASSWORD 'postgres'"
-bash -c 'pg_restore -d adb /adb_dump/adb-v0.1.1.dump' || true
+psql -c 'DROP DATABASE IF EXISTS adb'
+psql -c 'CREATE DATABASE adb'
+bash -c 'pg_restore -O -d adb /adb_dump/adb-v0.1.1.dump' || true
--- docker/bigg_docker/scripts/restore_biggdb.sh
+++ docker/bigg_docker/scripts/restore_biggdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-createuser -s zaking
-createdb bigg
-psql -c "ALTER USER postgres WITH PASSWORD 'postgres'"
-bash -c 'pg_restore -d bigg /bigg_database_dump/bigg_database.dump' || true
+psql -c 'DROP DATABASE IF EXISTS bigg'
+psql -c 'CREATE DATABASE bigg'
+bash -c 'pg_restore -O -d bigg /bigg_database_dump/bigg_database.dump' || true
