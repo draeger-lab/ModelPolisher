fa4fdff632f3ffddfb6c71b2e1c4768336fde8b2
codekaust <kaustubh1303@gmail.com>
2019-08-05 13:48:22 +0530

    fix #56

--- src/main/java/edu/ucsd/sbrg/bigg/AnnotateDB.java
+++ src/main/java/edu/ucsd/sbrg/bigg/AnnotateDB.java
@@ -31,6 +31,12 @@ public class AnnotateDB {
      */
     AnnotateDB(SQLConnector connector) throws SQLException {
         this.connector = connector;
+    }
+
+    /**
+     *
+     */
+    void startConnection() throws SQLException {
         if (!connector.isConnected()) {
             connector.connect();
         }
@@ -49,22 +55,24 @@ public class AnnotateDB {
         }
     }
 
-    public TreeSet<String>  getAnnotations(String type, String biggId){
+    public TreeSet<String> getAnnotations(String type, String biggId) {
         TreeSet<String> annotations = new TreeSet<>();
 
-        if(!type.equals(BIGG_METABOLITE) && !type.equals(BIGG_REACTION)){
+        if (!type.equals(BIGG_METABOLITE) && !type.equals(BIGG_REACTION)) {
             return annotations;
         }
 
-        if(type.equals(BIGG_METABOLITE) && biggId.substring(0,2).equals("M_")) biggId = biggId.substring(2);
-        if(type.equals(BIGG_METABOLITE) && biggId.substring(0,2).equals("R_")) biggId = biggId.substring(2);
-        if(biggId.substring(biggId.length()-2,biggId.length()-1).equals("_")) biggId = biggId.substring(0,biggId.length()-2);
+        if (type.equals(BIGG_METABOLITE) && biggId.substring(0, 2).equals("M_")) biggId = biggId.substring(2);
+        if (type.equals(BIGG_METABOLITE) && biggId.substring(0, 2).equals("R_")) biggId = biggId.substring(2);
+        if (biggId.substring(biggId.length() - 2, biggId.length() - 1).equals("_"))
+            biggId = biggId.substring(0, biggId.length() - 2);
 
         String query = SELECT + "m." + COLUMN_TARGET_TERM + ", ac." + COLUMN_URLPATTERN + FROM + MAPPING_VIEW + " m, " + ADB_COLLECTION + " ac" + WHERE +
                 "m." + COLUMN_SOURCE_NAMESPACE + " = '" + type + "' AND " + "m." + COLUMN_SOURCE_TERM + " = '" + biggId + "' AND ac." + COLUMN_NAMESPACE + " = m." + COLUMN_TARGET_NAMESPACE;
 
 
         try {
+            startConnection();
             ResultSet rst = connector.query(query);
             while (rst.next()) {
                 String uri = rst.getString(COLUMN_URLPATTERN);
@@ -72,6 +80,7 @@ public class AnnotateDB {
                 uri = uri.replace("{$id}", id);
                 annotations.add(uri);
             }
+            closeConnection();
         } catch (SQLException exc) {
             logger.warning(Utils.getMessage(exc));
         }
