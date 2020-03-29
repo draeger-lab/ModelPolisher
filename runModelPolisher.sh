48e87aa6f5148d5c345084509b1ae30ed4b5d81e
Thomas Jakob Zajac <zajac.thomas1992@gmail.com>
2020-03-29 07:22:08 +0200

    Updated docker-compose for release and pushed current polisher image to Dockerhub. Added a bash script to run dockerized ModelPolisher with BiGGAnnotations and offline validation.

---
+++ runModelPolisher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -x "$(command -v docker)" ]; then echo "Docker is not installed, please set up docker"; exit -1; fi
+MODELS=$1
+if [ -z ${MODELS} ]; then echo "Path to model folder is either empty or not set. Please set correct path"; exit -2; fi
+if [ ! -d ${MODELS} ]; then echo "Path does not point to directory: ${MODELS}"; exit -3; fi
+docker-compose run -u $(id -u):$(id -g) -v ${MODELS}:/models polisher java -Djava.util.prefs.userRoot=/ -jar /ModelPolisher-2.1-beta.jar --input=/models/ --output=/models/out --annotate-with-bigg=true --sbml-validation=true --compression-type=GZIP
