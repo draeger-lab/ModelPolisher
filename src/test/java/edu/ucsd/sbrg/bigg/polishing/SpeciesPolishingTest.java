5ab2fd5fcaca6be945a74168b12638a7df49e51a
Schmoho <d.eltzner@gmx.de>
2022-07-05 19:05:49 +0200

    issue #121: cleaning out tests

--- src/test/java/edu/ucsd/sbrg/bigg/polishing/SpeciesPolishingTest.java
+++ src/test/java/edu/ucsd/sbrg/bigg/polishing/SpeciesPolishingTest.java
@@ -1,13 +1,36 @@
 package edu.ucsd.sbrg.bigg.polishing;
 
 import org.junit.jupiter.api.Test;
-import org.sbml.jsbml.ListOf;
 import org.sbml.jsbml.Model;
-import org.sbml.jsbml.Species;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SpeciesPolishingTest {
+
+    @Test
+    public void noIdNoChanges() {
+        var m = new Model(3, 2);
+        var s = m.createSpecies();
+
+        assertEquals(0, m.getCompartmentCount());
+        assertEquals(false, s.isSetConstant());
+        assertEquals(false, s.isSetBoundaryCondition());
+        assertEquals(false, s.isSetHasOnlySubstanceUnits());
+        assertEquals(1,  m.getSpeciesCount());
+
+        var polishing = new SpeciesPolishing(s);
+        var deleteTask = polishing.polish();
+
+        assertTrue(deleteTask.isPresent());
+        assertEquals(0, m.getCompartmentCount());
+        assertEquals(false, s.isSetMetaId());
+        assertEquals(false, s.isSetId());
+        assertEquals(false, s.isSetConstant());
+        assertEquals(false, s.isSetBoundaryCondition());
+        assertEquals(false, s.isSetHasOnlySubstanceUnits());
+        assertEquals(1,  m.getSpeciesCount());
+    }
 
     /**
      * Default values according to the SBML spec are set and the model is aware of it.
@@ -15,21 +38,18 @@ public class SpeciesPolishingTest {
     @Test
     public void defaultsAreSet() {
         var m = new Model(3, 2);
+        var s = m.createSpecies("stuff_c");
 
-        var ls = new ListOf<Species>(3, 2);
-        ls.setParent(m);
-        var s = new Species(3, 2);
-        s.setId("stuff_c");
-        ls.add(s);
-
-        assertEquals(0, m.getListOfCompartments().getChildCount());
+        assertEquals(0, m.getCompartmentCount());
         assertEquals(false, s.isSetConstant());
         assertEquals(false, s.isSetBoundaryCondition());
         assertEquals(false, s.isSetHasOnlySubstanceUnits());
+        assertEquals(1,  m.getSpeciesCount());
 
         var polishing = new SpeciesPolishing(s);
-        polishing.polish();
+        var deleteTask = polishing.polish();
 
+        assertTrue(deleteTask.isEmpty());
         assertEquals("c", s.getCompartment());
         assertEquals("stuff_c", s.getId());
         assertEquals("default", m.getListOfCompartments().get(0).getName());
@@ -40,6 +60,7 @@ public class SpeciesPolishingTest {
         assertEquals(false, s.isBoundaryCondition());
         assertEquals(true, s.isSetHasOnlySubstanceUnits());
         assertEquals(true, s.hasOnlySubstanceUnits());
+        assertEquals(1,  m.getSpeciesCount());
     }
 
     /**
@@ -50,23 +71,21 @@ public class SpeciesPolishingTest {
     @Test
     public void compartmentIsSetFromId() {
         var m = new Model(3, 2);
-
-        var ls = new ListOf<Species>(3, 2);
-        ls.setParent(m);
-        var s = new Species(3, 2);
+        var s = m.createSpecies("stuff_c");
         s.setCompartment("e");
-        s.setId("stuff_c");
-        ls.add(s);
 
-        assertEquals(0, m.getListOfCompartments().getChildCount());
+        assertEquals(0, m.getCompartmentCount());
+        assertEquals(1,  m.getSpeciesCount());
 
         var polishing = new SpeciesPolishing(s);
-        polishing.polish();
+        var deleteTask = polishing.polish();
 
+        assertTrue(deleteTask.isEmpty());
         assertEquals("c", s.getCompartment());
         assertEquals("stuff_c", s.getId());
         assertEquals("default", m.getListOfCompartments().get(0).getName());
         assertEquals("c", m.getListOfCompartments().get(0).getId());
+        assertEquals(1,  m.getSpeciesCount());
     }
 
     /**
@@ -77,19 +96,15 @@ public class SpeciesPolishingTest {
     @Test
     public void nonCompartmentIdIsNotSetToCompartmentAttribute() {
         var m = new Model(3, 2);
-
-        var ls = new ListOf<Species>(3, 2);
-        ls.setParent(m);
-        var s = new Species(3, 2);
+        var s = m.createSpecies("stuff");
         s.setCompartment("e");
-        s.setId("stuff");
-        ls.add(s);
 
-        assertEquals(0, m.getListOfCompartments().getChildCount());
+        assertEquals(0, m.getCompartmentCount());
 
         var polishing = new SpeciesPolishing(s);
-        polishing.polish();
+        var deleteTask = polishing.polish();
 
+        assertTrue(deleteTask.isEmpty());
         assertEquals("e", s.getCompartment());
         assertEquals("stuff", s.getId());
         assertEquals("default", m.getListOfCompartments().get(0).getName());
