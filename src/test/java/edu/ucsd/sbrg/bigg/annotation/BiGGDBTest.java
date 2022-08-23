ffa4e67acb5c40f645cc787d583da0959f350f1d
Schmoho <d.eltzner@gmx.de>
2022-08-23 12:46:40 +0200

    sparser logging + fix container testing setup for gradle runs

--- src/test/java/edu/ucsd/sbrg/bigg/annotation/BiGGDBTest.java
+++ src/test/java/edu/ucsd/sbrg/bigg/annotation/BiGGDBTest.java
@@ -1,10 +1,7 @@
 package edu.ucsd.sbrg.bigg.annotation;
 
 import edu.ucsd.sbrg.db.BiGGDB;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
@@ -13,22 +10,14 @@ import java.time.Duration;
 @Testcontainers
 public abstract class BiGGDBTest {
 
-
-    @BeforeEach
-    public void setup() {
-        BiGGDB.init(bigg.getHost(), bigg.getFirstMappedPort().toString(), "postgres", "postgres", "bigg");
-    }
-
-    @AfterEach
-    public void cleanUp() {
-        if (BiGGDB.inUse())
-            BiGGDB.close();
-    }
-
-    @Container
-    public GenericContainer bigg = new GenericContainer(DockerImageName.parse("preloaded_bigg:latest"))
+    static final GenericContainer bigg = new GenericContainer(DockerImageName.parse("preloaded_bigg:latest"))
             .withExposedPorts(5432)
             .withEnv("POSTGRES_PASSWORD", "postgres")
             .withStartupTimeout(Duration.ofMinutes(5));
+
+    static {
+        bigg.start();
+        BiGGDB.init(bigg.getHost(), bigg.getFirstMappedPort().toString(), "postgres", "postgres", "bigg");
+    }
 
 }
